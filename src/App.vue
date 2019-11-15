2a3f4e2b9edb33c3997583555f56c90a07c8fd5a
andrew.mandruk@runsel.com <andrew.mandruk@runsel.com>
2019-11-15 22:47:52 +0200

    Page reload on back button add

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,15 @@
 <template>
   <div v-if="currentPage <= totalPages" id="app" class="app">
     <router-view/>
+
     <div class="pagination">
+      <router-link :to="`/${prevPage}`" v-if="isPrevPageExist">
+        <md-button class="md-raised">Prev</md-button>
+      </router-link>
+      <md-button v-else class="md-raised" disabled>
+        Prev
+      </md-button>
+
       <router-link
         v-for="page in totalPages"
         :to="`/${page}`"
@@ -9,23 +17,26 @@
         :disabled="page === currentPage"
       >
         <md-button
-          :md-ripple="false"
           class="md-raised"
           :class="{ 'md-primary' : page === currentPage }"
-          @click="getUsers(page)"
         >
           {{ page }}
         </md-button>
       </router-link>
+
+      <router-link :to="`/${nextPage}`" v-if="isNextPageExist">
+        <md-button class="md-raised">Next</md-button>
+      </router-link>
+      <md-button v-else class="md-raised" disabled>
+        Next
+      </md-button>
     </div>
   </div>
+
   <div class="no-page" v-else>
     <h2>This page doesn't exist</h2>
     <router-link  to="/1">
-      <md-button
-        class="md-raised"
-        @click="getUsers(1)"
-      >
+      <md-button class="md-raised" @click="getUsers(1)">
         Go to the first page
       </md-button>
     </router-link>
@@ -36,19 +47,33 @@
 import { mapActions, mapState } from 'vuex'
 
 export default {
-  data () {
-    return {
-      path: this.$router.currentRoute.path.slice(1)
-    }
-  },
   created () {
-    if (!isNaN(this.path)) this.getUsers(+this.path)
+    const path = this.$router.currentRoute.path.slice(1)
+    const isPositiveNumber = !isNaN(path) && +path > 0
+    if (isPositiveNumber) this.getUsers(+path)
   },
   methods: {
     ...mapActions(['getUsers'])
   },
   computed: {
-    ...mapState(['currentPage', 'totalPages'])
+    ...mapState(['currentPage', 'totalPages']),
+    prevPage () {
+      return this.currentPage - 1
+    },
+    isPrevPageExist () {
+      return this.prevPage > 0
+    },
+    nextPage () {
+      return this.currentPage + 1
+    },
+    isNextPageExist () {
+      return this.nextPage <= this.totalPages
+    }
+  },
+  watch: {
+    '$route' (to, from) {
+      this.getUsers(to.path.slice(1))
+    }
   }
 }
 
@@ -64,6 +89,8 @@ export default {
 .pagination {
   display: flex;
   flex-wrap: wrap;
+  justify-content: center;
+  margin-top: 20px;
 }
 
 .no-page {
